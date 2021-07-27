cfea310fb67cf4dfd9d418d343a0b6ceafba8863
leelixiangjun <leelixiangjun@didiglobal.com>
2021-07-27 14:21:10 +0800

    2021年07月27日

---
+++ src/com/LeetCode/IsCovered.java
@@ -0,0 +1,22 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author leelixiangjun
+ * @date 2021/7/23 15:32
+ */
+public class IsCovered {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+
+        for (int i=left;i<=right;i++){
+        }
+        return true;
+    }
+}
--- src/com/LeetCode/TreeNode.java
+++ src/com/LeetCode/TreeNode.java
@@ -1,10 +1,20 @@
 package com.LeetCode;
 
-class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode(int _val) {
-        val = _val;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
