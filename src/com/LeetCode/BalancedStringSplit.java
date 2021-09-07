c6d964d35b837cf1dd4457259f5cfb4a3363902b
leelixiangjun <leelixiangjun@didiglobal.com>
2021-09-07 17:05:32 +0800

    BalancedStringSplit

---
+++ src/com/LeetCode/BalancedStringSplit.java
@@ -0,0 +1,17 @@
+package com.LeetCode;
+
+/**
+ * @author leelixiangjun
+ * @date 2021/9/7 16:53
+ */
+public class BalancedStringSplit {
+    public int balancedStringSplit(String s) {
+        int result = 0, delta = 0;
+        char[] words = s.toCharArray();
+        for (char c : words) {
+            delta = delta + (c == 'L' ? 1 : -1);
+            result = result + (delta == 0 ? 1 : 0);
+        }
+        return result;
+    }
+}
