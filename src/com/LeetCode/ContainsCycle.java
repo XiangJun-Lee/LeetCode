e53dc434d846d39a7f4a92c864985de5dd9869d7
leelixiangjun <leelixiangjun@didiglobal.com>
2021-10-14 10:46:18 +0800

    https://leetcode-cn.com/problems/data-stream-as-disjoint-intervals/

---
+++ src/com/LeetCode/ContainsCycle.java
@@ -0,0 +1,21 @@
+//package com.LeetCode;
+//
+///**
+// * @author leelixiangjun
+// * @date 2021/10/11 19:36
+// * @link https://leetcode-cn.com/problems/detect-cycles-in-2d-grid/
+// */
+//public class ContainsCycle {
+//    public boolean containsCycle(char[][] grid) {
+//        for (int i=0;i<grid.length;i++){
+//            for (int j=0;j<grid[i].length;j++){
+//                char c = grid[i][j];
+//                if (c =='0'){
+//                    continue;
+//                }
+//                grid[i][j] = '0';
+//
+//            }
+//        }
+//    }
+//}
---
+++ src/com/LeetCode/PeakIndexInMountainArray.java
@@ -0,0 +1,30 @@
+package com.LeetCode;
+
+import org.junit.Test;
+
+/**
+ * @author leelixiangjun
+ * @date 2021/10/14 10:37
+ */
+public class PeakIndexInMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1] && arr[mid - 1] < arr[mid]) {
+                left = mid;
+            } else if (arr[mid] > arr[mid + 1] && arr[mid - 1] > arr[mid]) {
+                right = mid;
+            } else if (arr[mid] > arr[mid + 1] && arr[mid - 1] < arr[mid]) {
+                return mid;
+            }
+        }
+        return left;
+    }
+
+    @Test
+    public void test(){
+        int[] arr = new int[]{0,2,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+}
