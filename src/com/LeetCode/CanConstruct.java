50d37361d2d99042feeff27b2a3511eaa7c61967
leelixiangjun <leelixiangjun@didiglobal.com>
2021-12-07 15:25:30 +0800

    easy

---
+++ src/com/LeetCode/CanConstruct.java
@@ -0,0 +1,30 @@
+package com.LeetCode;
+
+import org.junit.Test;
+
+/**
+ * @author leelixiangjun
+ * @date 2021/12/7 14:23
+ */
+public class CanConstruct {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] letter = new char[26];
+        for (char l : magazine.toCharArray()) {
+            letter[l - 'a']++;
+        }
+        for (char l : ransomNote.toCharArray()) {
+            if (letter[l - 'a'] == 0) {
+                return false;
+            }
+            letter[l - 'a']--;
+        }
+        return true;
+    }
+
+    @Test
+    public void test(){
+        String ransomNote = "a";
+        String magazine = "b";
+        System.out.println(canConstruct(ransomNote,magazine));
+    }
+}
---
+++ src/com/LeetCode/SuperPow.java
@@ -0,0 +1,42 @@
+package com.LeetCode;
+
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+/**
+ * @author leelixiangjun
+ * @date 2021/12/7 11:33
+ */
+public class SuperPow {
+    private final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        int res = 1;
+        for (int i = b.length - 1; i >= 0; i--) {
+            res = ((res % MOD) * (pow(a, b[i]) % MOD)) % MOD;
+            a = pow(a, 10);
+        }
+        return res;
+    }
+
+    private int pow(int x, int n) {
+        int res = 1;
+        while (n != 0) {
+            if (n % 2 != 0) {
+                res = ((res % MOD) * (x % MOD));
+            }
+            x = x % MOD;
+            x = x * x;
+            n /= 2;
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        int a = 2147483647;
+        int[] b = {2, 0, 0};
+        System.out.println(superPow(a, b));
+    }
+}
