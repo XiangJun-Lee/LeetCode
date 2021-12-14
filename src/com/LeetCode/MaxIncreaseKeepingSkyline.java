11572da687792733a128b77807902ef3ca61c8d5
leelixiangjun <leelixiangjun@didiglobal.com>
2021-12-14 23:46:14 +0800

    hard

---
+++ src/com/LeetCode/MaxIncreaseKeepingSkyline.java
@@ -0,0 +1,35 @@
+package com.LeetCode;
+
+import org.junit.Test;
+
+/**
+ * @author leeixiangjun
+ * @date 2021/12/13 11:08 下午
+ */
+public class MaxIncreaseKeepingSkyline {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[i] = Math.max(colMax[i], grid[j][i]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += (Math.min(rowMax[j], colMax[i])-grid[i][j]);
+            }
+        }
+        return ans;
+    }
+
+    @Test
+    public void test(){
+        int[][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+        System.out.println(maxIncreaseKeepingSkyline(grid));
+    }
+
+}
