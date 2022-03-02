d705353e6505e65a8cafbb88044a080b6969b016
leelixiangjun <leelixiangjun@didiglobal.com>
2022-03-02 11:46:03 +0800

    hard

--- src/com/LeetCode/March/March1.java
+++ src/com/LeetCode/March/March1.java
@@ -32,42 +32,5 @@ public class March1 {
             res.append(row);
         }
         return res.toString();
- * @author leelixiangjun
- * @date 2022/3/1 20:17
- */
-public class March1 {
-    public String convert(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
-        int len = s.length(), start = 0, time = 0;
-        List<char[]> sub = new ArrayList<>();
-        while (start < len) {
-            int end = start + numRows - (time % 2 == 0 ? 0 : 2);
-            end = Math.min(end, len);
-            String subString = s.substring(start, end);
-            if (time % 2 == 1) {
-                subString = new StringBuilder(subString).reverse().toString();
-            }
-            sub.add(subString.toCharArray());
-            start = end;
-            time++;
-        }
-        StringBuilder resp = new StringBuilder();
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < sub.size(); j++) {
-                if (j % 2 == 1 && (i == 0 || i == numRows - 1)) {
-                    continue;
-                }
-
-                char[] chars = sub.get(j);
-                if (i >= chars.length) {
-                    continue;
-                }
-                resp.append(chars[i]);
-            }
-        }
-        return resp.toString();
     }
-        System.out.println(convert("ABCDE", 4));
 }
