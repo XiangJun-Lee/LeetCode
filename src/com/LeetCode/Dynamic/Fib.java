bf4f9a042e5ae61086c5ce4ccc4e280baa871553
leelixiangjun <leelixiangjun@didiglobal.com>
2022-03-02 21:01:31 +0800

    动态规划

---
+++ src/com/LeetCode/Dynamic/Fib.java
@@ -0,0 +1,27 @@
+package com.LeetCode.Dynamic;
+
+import org.junit.Test;
+
+/**
+ * @author leelixiangjun
+ * @date 2022/3/2 20:22
+ */
+public class Fib {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p, q = 0, r = 1;
+        for (int i = 2; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(fib(4));
+    }
+}
---
+++ src/com/LeetCode/Dynamic/Tribonacci.java
@@ -0,0 +1,24 @@
+package com.LeetCode.Dynamic;
+
+/**
+ * @author leelixiangjun
+ * @date 2022/3/2 20:32
+ */
+public class Tribonacci {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; i++) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+}
