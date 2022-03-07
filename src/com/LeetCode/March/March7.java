1906ea205dfde6e0808efd4631d04e06a8549a94
leelixiangjun <leelixiangjun@didiglobal.com>
2022-03-07 10:09:21 +0800

    7号

---
+++ src/com/LeetCode/March/March7.java
@@ -0,0 +1,20 @@
+package com.LeetCode.March;
+
+/**
+ * @author leelixiangjun
+ * @date 2022/3/7 10:04
+ */
+public class March7 {
+    public String convertToBase7(int num) {
+        if (num < 0) {
+            return "-" + convertToBase7(-num);
+        }
+        StringBuilder ans = new StringBuilder();
+        while (num > 7) {
+            ans.append(num % 7);
+            num /= 7;
+        }
+        ans.append(num);
+        return ans.reverse().toString();
+    }
+}
