484a7559a94393147ff7c69c559e180e6f549b3e
leelixiangjun <leelixiangjun@didiglobal.com>
2022-03-07 16:29:47 +0800

    动态规划

---
+++ src/com/LeetCode/Dynamic/MaxSubarraySumCircular.java
@@ -0,0 +1,27 @@
+package com.LeetCode.Dynamic;
+
+import org.junit.Test;
+
+/**
+ * @author leelixiangjun
+ * @date 2022/3/7 15:16
+ */
+public class MaxSubarraySumCircular {
+    public int maxSubarraySumCircular(int[] nums) {
+        int maxPre = 0, maxAns = nums[0], minPre = 0, minAns = nums[0], total = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            maxPre = Math.max(maxPre + nums[i], nums[i]);
+            maxAns = Math.max(maxAns, maxPre);
+            minPre = Math.min(minPre + nums[i], nums[i]);
+            minAns = Math.min(minAns, minPre);
+            total += nums[i];
+        }
+        return maxAns > 0 ? Math.max(maxAns, total - minAns) : maxAns;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {5, -3, 5};
+        System.out.println(maxSubarraySumCircular(nums));
+    }
+}
