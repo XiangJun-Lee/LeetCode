7e1123b9c51ca65ff478a89634b58cb411cdd211
leelixiangjun <leelixiangjun@didiglobal.com>
2022-04-13 10:27:24 +0800

    每日一题13号 mid 使用哈希+链表(变长数组) 学习list.set()

---
+++ src/com/LeetCode/April/April12.java
@@ -0,0 +1,33 @@
+package com.LeetCode.April;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author leelixiangjun
+ * @date 2022/4/12 17:39
+ */
+public class April12 {
+
+    public int[] numberOfLines(int[] widths, String s) {
+        int curLength = 0;
+        int countLine = 1;
+        for (char c : s.toCharArray()) {
+            int length = widths[c - 'a'];
+            if (curLength + length > 100) {
+                countLine++;
+                curLength = 0;
+            }
+            curLength += length;
+        }
+        return new int[]{countLine, curLength};
+    }
+
+    @Test
+    public void test(){
+        int[] widths = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+        String S = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(Arrays.toString(numberOfLines(widths, S)));
+    }
+}
